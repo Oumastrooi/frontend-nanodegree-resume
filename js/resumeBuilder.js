8b6624fa0b4c802c5e5467cac5e2622cb2d8250d
Oumastrooi <e_g172@txstate.edu>
2017-10-17 20:06:39 -0500

    code review changes

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,5 @@
+var data = '%data%';
+
 var bio = {
         "name": "Elize Geldenhuys",
         "role": "Front End Developer",
@@ -11,17 +13,17 @@ var bio = {
         "skills": ["HTML/CSS", "Javascript", "Angular 4", "Website Design"],
         "bioPic": "images/fry.jpg",
         display: function() {
-            $("#topContacts").append(HTMLmobile.replace("%data%", bio.contacts.mobile));
-            $("#topContacts").append(HTMLemail.replace("%data%", bio.contacts.email));
-            $("#topContacts").append(HTMLgithub.replace("%data%", bio.contacts.github));
-            $("#topContacts").append(HTMLlocation.replace("%data%", bio.contacts.location));
-            $("#header").prepend(HTMLheaderRole.replace("%data%", bio.role));
-            $("#header").prepend(HTMLheaderName.replace("%data%", bio.name));
-            $("#header").append(HTMLbioPic.replace("%data%", bio.bioPic));
-            $("#header").append(HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage));
+            $("#footerContacts").append(HTMLmobile.replace(data, bio.contacts.mobile));
+            $("#footerContacts").append(HTMLemail.replace(data, bio.contacts.email));
+            $("#footerContacts").append(HTMLgithub.replace(data, bio.contacts.github));
+            $("#footerContacts").append(HTMLlocation.replace(data, bio.contacts.location));
+            $("#header").prepend(HTMLheaderRole.replace(data, bio.role));
+            $("#header").prepend(HTMLheaderName.replace(data, bio.name));
+            $("#header").append(HTMLbioPic.replace(data, bio.bioPic));
+            $("#header").append(HTMLwelcomeMsg.replace(data, bio.welcomeMessage));
             $("#header").append(HTMLskillsStart);
             bio.skills.forEach(function(skill){
-                $('#skills').append(HTMLskills.replace("%data%", skill));
+                $('#skills').append(HTMLskills.replace(data, skill));
             });
         }
 };
@@ -50,10 +52,10 @@ var work = {
             $('#workExperience').append(HTMLworkStart);
 
             var formattedJob =
-                HTMLworkEmployer.replace("%data%", job.employer) + HTMLworkTitle.replace("%data%", job.title) +
-                HTMLworkDates.replace("%data%", job.dates) +
-                HTMLworkLocation.replace("%data%", job.location) +
-                HTMLworkDescription.replace("%data%", job.description);
+                HTMLworkEmployer.replace(data, job.employer) + HTMLworkTitle.replace(data, job.title) +
+                HTMLworkDates.replace(data, job.dates) +
+                HTMLworkLocation.replace(data, job.location) +
+                HTMLworkDescription.replace(data, job.description);
 
             $('.work-entry:last').append(formattedJob);
         });
@@ -67,43 +69,43 @@ var projects = {
         "title": "Terra by Osprey",
         "dates": "Fall 2016",
         "description": "Product Design",
-        "images": "images/terra.png"
+        "images": 'http://via.placeholder.com/150x150'
     },
     {
         "title": "Tempo for Intuit",
         "dates": "Fall 2016",
         "description": "Mobile Design, App Design",
-        "images": "images/tempo.png"
+        "images": 'http://via.placeholder.com/150x150'
     },
     {
         "title": "Behind the Design",
         "dates": "Spring 2017",
         "description": "Front End Development",
-        "images": "images/behind-the-design.png"
+        "images": 'http://via.placeholder.com/150x150'
     },
     {
         "title": "cumulo",
         "dates": "Spring 2017",
         "description": "Front End Development",
-        "images": "images/cumulo.png"
+        "images": 'http://via.placeholder.com/150x150'
     },
     {
         "title": "Article Mockup",
         "dates": "Summer 2017",
         "description": "Front End Development",
-        "images": "images/article-mockup-png"
+        "images": 'http://via.placeholder.com/150x150'
     },
     {
         "title": "Animal Trading Cards",
         "dates": "Summer 2017",
         "description": "Front End Development",
-        "images": "images/animal-trading-card.png"
+        "images": 'http://via.placeholder.com/150x150'
     },
     {
         "title": "Portfolio Site",
         "dates": "Summer 2017",
         "description": "Front End Development",
-        "images": "images/portfolio-site.png"
+        "images": 'http://via.placeholder.com/150x150'
     }
 ],
     display: function() {
@@ -111,9 +113,9 @@ var projects = {
             $('#projects').append(HTMLprojectStart);
 
             var formattedProject =
-                HTMLprojectTitle.replace("%data%", project.title) + HTMLprojectDates.replace("%data%", project.dates) +
-                HTMLprojectDescription.replace("%data%", project.description) +
-                HTMLprojectImage.replace("%data%", project.image);
+                HTMLprojectTitle.replace(data, project.title) + HTMLprojectDates.replace(data, project.dates) +
+                HTMLprojectDescription.replace(data, project.description) +
+                HTMLprojectImage.replace(data, project.images);
 
             $('.project-entry:last').append(formattedProject);
         });
@@ -128,14 +130,14 @@ var education = {
         "name": "Texas State University",
         "location": "San Marcos, Tx",
         "degree": "B.A",
-        "majors": "Computer Science",
+        "majors": ["Computer Science"],
         "dates": "2012-2017"
         },
         {
         "name": "Texas State University",
         "location": "San Marcos, Tx",
         "degree": "B.F.A",
-        "majors": "Graphic Design",
+        "majors": ["Graphic Design"],
         "dates": "2012-2017"
         }
     ],
@@ -154,30 +156,28 @@ var education = {
         }
     ],
     display: function() {
+        $('#education').append(HTMLschoolStart);
         education.schools.forEach(function(school){
-            $('#education').append(HTMLschoolStart);
-
             var formattedSchool =
-                HTMLschoolName.replace("%data%", school.name) + HTMLschoolDegree.replace("%data%", school.degree) +
-                HTMLschoolDates.replace("%data%", school.dates) +
-                HTMLschoolLocation.replace("%data%", school.location) +
-                HTMLschoolMajor.replace("%data%", school.majors);
+                HTMLschoolName.replace(data, school.name) + HTMLschoolDegree.replace(data, school.degree) +
+                HTMLschoolDates.replace(data, school.dates) +
+                HTMLschoolLocation.replace(data, school.location) +
+                HTMLschoolMajor.replace(data, school.majors);
 
             $('.education-entry:last').append(formattedSchool);
         });
 
+        $('.education-entry:last').append(HTMLonlineClasses);
         education.onlineCourses.forEach(function(school){
-            $('#education').append(HTMLonlineClasses);
             var formattedSchool =
-                HTMLonlineTitle.replace("%data%", school.title) + HTMLonlineSchool.replace("%data%", school.school) +
-                HTMLonlineDates.replace("%data%", school.dates) +
-                HTMLonlineURL.replace("%data%", school.url);
-            $('.education-entry:last').append(formattedSchool);
+                HTMLonlineTitle.replace(data, school.title) + HTMLonlineSchool.replace(data, school.school) +
+                HTMLonlineDates.replace(data, school.dates) +
+                HTMLonlineURL.replace(data, school.url);
+            $('.education-entry').append(formattedSchool);
         });
-
-
-
     }
 };
 
 education.display();
+
+$('#mapDiv').append(googleMap);
